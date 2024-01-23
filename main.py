b330a9c0b2c8f51bc8d103effaed871ce314a65f
Ian <ian.thulin@icloud.com>
2024-01-23 11:48:11 -0700

    Framework for handling data created

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import sys
 import os
+import csv
 
 from PyQt6.QtWidgets import (QWidget, QLabel, QPushButton, QLineEdit, QTextEdit, QGridLayout, QApplication)
 from PyQt6.QtCore import Qt
+
+# Constants
+filepath = 'Data\Total DNC 1.9.24.csv'
+contacted_file_path = 'Data\Contacted.csv'
 
 class MainWindow(QWidget):
 
@@ -13,16 +18,27 @@ class MainWindow(QWidget):
 
     def initUI(self):
 
+        # Default Values
+        self.prospect = "Prospect #"
+        self.firstname = "First Name"
+        self.lastname = "Last Name"
+        self.address = "Address"
+        self.city = "City"
+        self.state = "State"
+        self.zipcode = "Zipcode"
+        self.phone = "Phone"
+        self.productid = "Product"
+
         # Static labels
-        prosp = QLabel("Prospect #:")
-        fname = QLabel("First Name:")
-        lname = QLabel("Last Name:")
-        address = QLabel("Address:")
-        city = QLabel("City:")
-        state = QLabel("State:")
-        zipcode = QLabel("Zip Code:")
-        product = QLabel("Product:")
-        phone = QLabel("Phone #:")
+        prosp = QLabel(self.prospect)
+        fname = QLabel(self.firstname)
+        lname = QLabel(self.lastname)
+        address = QLabel(self.address)
+        city = QLabel(self.city)
+        state = QLabel(self.state)
+        zipcode = QLabel(self.zipcode)
+        product = QLabel(self.zipcode)
+        phone = QLabel(self.phone)
 
         # Dynamic labels
         prosp_info = QLabel("Prospect")
@@ -75,9 +91,61 @@ class MainWindow(QWidget):
         self.setWindowTitle("SSW Call Tracking")
         self.show()
 
+    def update_ui(self, entry):
+        self.prospect = entry['custnumber']
+        self.firstname = entry['firstname']
+        self.lastname = entry['lastname']
+        self.address = entry['Address1']
+        self.city = entry['City']
+        self.state = entry['State']
+        self.zipcode = entry['Zip']
+        self.phone = entry['Phone']
+        self.productid = entry['productid']
+
+def read_csv(file_path):
+    data = []
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return data
+
+def mark_contacted(data, entry_id):
+    for entry in data:
+        if entry['id'] == entry_id:
+            entry['contacted'] = True
+            break
+
+def write_csv(file_path, data):
+    fieldnames = data[0].keys()
+    with open(file_path, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+
+def create_contacted_csv(data, contacted_file_path):
+    contacted_data = [entry for entry in data if entry.get('contacted')]
+    write_csv(contacted_file_path), contacted_data
+
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
+    
+    # Get csv
+    data = read_csv(filepath)
+    for entry in data:
+        window.update_ui(entry)
+    
+    # Input LP login credentials
+
+    # Load next contact
+    # Navigate to LP profile for contact
+    # Generate new call and tag
+    
+    # After clicking get next, save previous result as contacted and fetch
+
+    
+    
     sys.exit(app.exec())
 
 if __name__ == '__main__':
