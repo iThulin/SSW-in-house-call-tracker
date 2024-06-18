95a6fa89f40d6f509c2d1e01b69c3043c0f4c09d
Ian <ian.thulin@icloud.com>
2024-06-18 17:07:06 -0600

    Graph generation completed

--- graph traversal.py
+++ graph traversal.py
@@ -221,7 +221,7 @@ map = np.ones((200, 300))*255
 q_start = (100, 150)
 q_goal = (np.random.randint(0, len(map)), np.random.randint(0, len(map[0])))
 q_new = (0,0)
-K_max = 100 # Max number of vertices in the RRT
+K_max = 1000 # Max number of vertices in the RRT
 delta_q = 10
 
 
@@ -234,7 +234,7 @@ plt.ion()
 fig, ax = plt.subplots()
 ax.imshow(map)
 ax.plot(q_goal[1], q_goal[0], 'y*')
-ax.plot(q_start[1], q_start[0], 'b*')
+ax.plot(q_start[1], q_start[0], 'g*')
 
 
 
@@ -244,70 +244,75 @@ def get_random_node():
     return rand_node
 
 def get_nearest_node(random_node):
-    distances = [((node[0] - random_node[0])**2 + (node[1] + random_node[1])**2,(node[0], node[1])) for node in G]
+    keys = list(G.keys())
+    #print(f'\nkeys: {keys}\n')
+    distances = [((node[0] - random_node[0])**2 + (node[1] - random_node[1])**2,(node[0], node[1])) for node in keys]
     nearest_index = distances.index(min(distances))
-    print(f'\nG: {G}')
-    print(f'Distances: {distances}')
-    print(f'Index: {nearest_index}')
-    print(f'tuple: {distances[nearest_index]}')
-    return distances[nearest_index][1]
+    for entry in G:
+        print(f'{entry}: {G[entry]}')
+    #print(f'\nDistances: {distances}')
+    #print(f'Index: {nearest_index}')
+    #print(f'tuple: {distances[nearest_index]}')
+    return distances[nearest_index][1]    
 
 
 def steer_node(q_near, q_rand, delta_q):
-    theta = np.arctan2(q_rand[0] - q_near[0], q_rand[1] - q_rand[1])
+    theta = np.arctan2(q_rand[0] - q_near[0], q_rand[1] - q_near[1])
     q_new = (q_near[0] + int(delta_q * np.sin(theta)), q_near[1] + int(delta_q * np.cos(theta)))
     return q_new
 
 print(f'Start: {q_start}, Goal: {q_goal}')
 
-while k < K_max and q_new != 1: #q_goal and k < K_max:
+while k < K_max and q_new != q_goal:
     
+    # Generate a random point, the nearest point, and the new point to add to graph
     q_rand = get_random_node()
-    print(f'q_rand: {q_rand}')
-
-    ax.plot(q_rand[1], q_rand[0], 'r.')
-
-    #q_near = nearest vertex to q_rand that is already in G
     q_near = get_nearest_node(q_rand)
-    print(f'q_near: {q_near}')
-
-    ax.plot(q_near[1], q_near[0], 'y.')
-
     q_new = steer_node(q_near, q_rand, delta_q)
+
+    # Print points in question
+    #print(f'q_rand: {q_rand}')
+    #print(f'q_near: {q_near}')
+    #print(f'q_new : {q_new}')
 
     dist_q_near2q_new = (q_near[0] - q_new[0])**2 + (q_near[1] - q_new[1])**2
     dist_q_rand2q_near = (q_rand[0] - q_near[0])**2 + (q_rand[1] - q_near[1])**2
 
-    print(f'dist_q_near2q_new: {dist_q_near2q_new}, dist_q_rand2q_near: {dist_q_rand2q_near}')
+    #print(f'dist_q_near2q_new: {dist_q_near2q_new}, dist_q_rand2q_near: {dist_q_rand2q_near}')
 
 
     #q_new = compute a new configuration by moving delta_q from q_near into the direction of q_rand
             # or use q_rand if closer to q_near than delta_q
+
+    if dist_q_rand2q_near < dist_q_near2q_new:
+        q_new = q_rand
+        #print(f'overide q_new: {q_new}')
+
+
+    # Check if q_new is < delta_q from goal
+    dist_q_new2q_goal = np.sqrt((q_new[0]-q_goal[0])**2+(q_new[1]-q_goal[1])**2)    
     
-    # If q_new < than delta_q from q_goal:
-        #q_new = q_goal
-
-    # If edge between qnear and q_new is collision free
-        # Add q_new to G
-        # Add an edge between q_near and q_new
-        # INcrement k
-    
-
-    # get neighbors of nearest nodes
-    neighbors = G[q_near]
-    print(neighbors)
-    neighbors.append(q_rand)
-    print(neighbors)
-
-    G[q_near] = neighbors
-
-    G[q_rand] = [q_near]
+    if dist_q_new2q_goal < delta_q:        
+        q_new = q_goal
 
 
-    k += 1
+
+    # If edge between q_near and q_new is collision free   
+    if 0 <= q_new[0] < len(map) and 0 <= q_new[1] < len(map):
+
+        # Add q_new to graph
+        G[q_near].append(q_new)
+        G[q_new] = [q_near]
+
+        # plot new point and line from q_near -> q_new
+        ax.plot(q_new[1], q_new[0], 'c.')
+        ax.plot([q_near[1], q_new[1]], [q_near[0], q_new[0]], 'c-')
+
+        # Increment number of vertices in graph
+        k += 1
 
     plt.draw()
-    plt.pause(0.5)
+    plt.pause(.1)
 
 plt.ioff()
 plt.show()
